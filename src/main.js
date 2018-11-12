a62258b189d96cce0f7048951ddd90ff00def8d3
zhc <1009600394@qq.com>
2018-11-12 15:37:27 +0800

    添加vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
-import './plugins/vuetify'
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+import   './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+// Vue.use(Vuetify,{
+//   iconfont:'mdi'
+// });
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
