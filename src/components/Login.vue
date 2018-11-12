75491ecde95f195814781e4bb89c68ebb1161cfc
zhc <1009600394@qq.com>
2018-11-12 10:49:28 +0800

    写了个最简单的登录页面

---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="login-form"  class="login">
+      {{ msg }}
+      <form >
+        <label>用户名</label>
+        <input name="username" v-model="username"/> {{username}}
+        <br/>
+        <label>密码</label>
+        <input name="password" type="password" v-model="password"/>{{password}}
+        <br/>
+        <button >登录</button>
+      </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  },
+    data:function () {
+        return {
+            username:'',
+            password:''
+        }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
