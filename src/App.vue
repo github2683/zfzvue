1f4627eb13569b7a4343cce85ae97a61eb3387e0
zhc <1009600394@qq.com>
2018-11-13 17:49:17 +0800

    菜单 可推拉

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app>
-    <transition name="left-menu">
-        <Navigation v-if="show" v-bind:drawerm="drawerm" v-on:show-menu="show = !show "></Navigation>
-    </transition>
 
-    <v-layout>
-      <v-toolbar app>
+      <Navigation class="leftmenu" v-bind:class="{leftshow:show,lefthide:!show}"  v-on:show-menu="show = !show "></Navigation>
+
+
+    <v-layout  class="topmenu" v-bind:class="{topshow:!show,tophide:show}">
+      <v-toolbar app style="position: absolute;">
         <v-toolbar-side-icon @click.stop="show = !show"></v-toolbar-side-icon>
-        <v-toolbar-title>系统名称{{show}}</v-toolbar-title>
+        <v-toolbar-title>系统菜单{{show?'打开':'关闭'}}</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
           <v-btn flat>超链接1</v-btn>
@@ -51,13 +51,62 @@ export default {
 </script>
 
 <style>
-  .left-menu-enter-active, .left-menu-leave-active {
+  .leftmenu-enter-active, .leftmenu-leave-active {
     transition: margin-left 1s;
   }
-  .left-menu-enter /* .fade-leave-active below version 2.1.8 */ {
+  .leftmenu-enter /* .fade-leave-active below version 2.1.8 */ {
     margin-left: 0px;
   }
-  .left-menu-leave-to{
+  .leftmenu-leave-to{
     margin-left: -300px;
+  }
+/*
+linear：
+线性过渡。等同于贝塞尔曲线(0.0, 0.0, 1.0, 1.0)
+ease：
+平滑过渡。等同于贝塞尔曲线(0.25, 0.1, 0.25, 1.0)
+ease-in：
+由慢到快。等同于贝塞尔曲线(0.42, 0, 1.0, 1.0)
+ease-out：
+由快到慢。等同于贝塞尔曲线(0, 0, 0.58, 1.0)
+ease-in-out：
+由慢到快再到慢。等同于贝塞尔曲线(0.42, 0, 0.58, 1.0)
+step-start：
+等同于 steps(1, start)
+step-end：
+等同于 steps(1, end)
+steps(<integer>[, [ start | end ] ]?)：
+接受两个参数的步进函数。第一个参数必须为正整数，指定函数的步数。第二个参数取值可以是start或end，指定每一步的值发生变化的时间点。第二个参数是可选的，默认值为end。
+cubic-bezier(<number>, <number>, <number>, <number>)：
+特定的贝塞尔曲线类型，4个数值需在[0, 1]区间内
+*/
+  .leftmenu,.topmenu,.v-navigation-drawer[data-booted="true"]{
+
+    transition-property: margin-left,width;
+
+    transition-duration: 0.5s;
+    transition-timing-function: ease-out ;
+    transition-delay: 10ms;
+    z-index: 10;
+    position: absolute;
+  }
+  .topmenu{
+    z-index: 10;
+    position: absolute;
+  }
+  .leftshow{
+    margin-left: 0px;
+  }
+  .lefthide{
+    margin-left: -300px;
+  }
+
+  .topshow{
+    margin-left: 0px;
+    width: 100%;
+  }
+  .tophide{
+    margin-left: 300px;
+    width: calc(100% - 300px);
   }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,109 +1,85 @@
 <template>
-    <!--<v-layout wrap style="height: 200px;" >-->
-        <!--<v-container>-->
-            <!--<v-layout justify-center>-->
-                <!--<v-btn color="pink" dark  @click.stop="drawer = !drawer" > 显示/隐藏 </v-btn>-->
-            <!--</v-layout>-->
-        <!--</v-container>-->
+    <v-navigation-drawer
+            stateless
+            value="true"
+    >
+        <v-list>
+            <v-list-tile style="height: 56px;">
+                <v-list-tile-action v-on:click="$emit('show-menu')">
+                    <v-icon>home</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-title>Home</v-list-tile-title>
+                <!--<v-list-tile-action v-on:click="$emit('show-menu')">-->
+                    <!--<v-icon style="margin-left:40px;cursor:pointer;">arrow_back_ios</v-icon>-->
+                <!--</v-list-tile-action>-->
+            </v-list-tile>
 
-        <v-navigation-drawer v-bind:class="{'menu-hide':mini}"
-                v-model="drawer"
-                absolute
-                dark
-        >
-            <!--temporary  蒙版 点击则收起  -->
-            <v-list class="pa-1">
-                <!--<v-list-tile v-if="mini" @click.stop="mini = !mini">-->
-                    <!--<v-list-tile-action>-->
-                        <!--<v-icon>chevron_right</v-icon>-->
-                    <!--</v-list-tile-action>-->
-                <!--</v-list-tile>-->
-
-                <v-list-tile avatar tag="div">
-                    <v-list-tile-avatar>
-                        <img src="https://randomuser.me/api/portraits/men/85.jpg">
-                    </v-list-tile-avatar>
-
-                    <v-list-tile-content>
-                        <v-list-tile-title>用户名</v-list-tile-title>
-                    </v-list-tile-content>
-
-                    <v-list-tile-action>
-                        <v-btn icon  v-on:click="$emit('show-menu')">
-                            <v-icon>chevron_left</v-icon>
-                        </v-btn>
-                    </v-list-tile-action>
+            <v-list-group
+                    prepend-icon="account_circle"
+                    value="true"
+            >
+                <v-list-tile slot="activator">
+                    <v-list-tile-title>Users</v-list-tile-title>
                 </v-list-tile>
-            </v-list>
 
-            <v-list class="pt-0" dense>
-                <v-divider light></v-divider>
-
-                <v-list-tile
-                        v-for="item in items"
-                        :key="item.title"
-                        @click=""
+                <v-list-group
+                        no-action
+                        sub-group
+                        value="true"
                 >
-                    <v-list-tile-action>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-tile-action>
+                    <v-list-tile slot="activator">
+                        <v-list-tile-title>Admin</v-list-tile-title>
+                    </v-list-tile>
 
-                    <v-list-tile-content>
-                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list>
-        </v-navigation-drawer>
-    <!--</v-layout>-->
+                    <v-list-tile
+                            v-for="(admin, i) in admins"
+                            :key="i"
+                            @click=""
+                    >
+                        <v-list-tile-title v-text="admin[0]"></v-list-tile-title>
+                        <v-list-tile-action>
+                            <v-icon v-text="admin[1]"></v-icon>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                </v-list-group>
+
+                <v-list-group
+                        sub-group
+                        no-action
+                >
+                    <v-list-tile slot="activator">
+                        <v-list-tile-title>Actions</v-list-tile-title>
+                    </v-list-tile>
+
+                    <v-list-tile
+                            v-for="(crud, i) in cruds"
+                            :key="i"
+                            @click=""
+                    >
+                        <v-list-tile-title v-text="crud[0]"></v-list-tile-title>
+                        <v-list-tile-action>
+                            <v-icon v-text="crud[1]"></v-icon>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                </v-list-group>
+            </v-list-group>
+        </v-list>
+    </v-navigation-drawer>
 </template>
 
 <script>
     export default {
-        props:['drawerm'],
-        name: "navigation-drawer",
-        data () {
-            return {
-                drawer: this.drawerm,
-                items: [
-                    { title: 'Home', icon: 'dashboard' },
-                    { title: 'About', icon: 'question_answer' }
-                ],
-                mini: false,
-                right: null
-            }
-        }
-
+        data: () => ({
+            admins: [
+                ['Management', 'people_outline'],
+                ['Settings', 'settings']
+            ],
+            cruds: [
+                ['Create', 'add'],
+                ['Read', 'insert_drive_file'],
+                ['Update', 'update'],
+                ['Delete', 'delete']
+            ]
+        })
     }
 </script>
-
-<style scoped>
-    .menu{
-        transition-property: margin-left; /* 要过渡的属性 */
-        transition-duration: .5s;/* 过渡持续时间 */
-        /*
-        linear：
-        线性过渡。等同于贝塞尔曲线(0.0, 0.0, 1.0, 1.0)
-        ease：
-        平滑过渡。等同于贝塞尔曲线(0.25, 0.1, 0.25, 1.0)
-        ease-in：
-        由慢到快。等同于贝塞尔曲线(0.42, 0, 1.0, 1.0)
-        ease-out：
-        由快到慢。等同于贝塞尔曲线(0, 0, 0.58, 1.0)
-        ease-in-out：
-        由慢到快再到慢。等同于贝塞尔曲线(0.42, 0, 0.58, 1.0)
-        step-start：
-        等同于 steps(1, start)
-        step-end：
-        等同于 steps(1, end)
-        steps(<integer>[, [ start | end ] ]?)：
-        接受两个参数的步进函数。第一个参数必须为正整数，指定函数的步数。第二个参数取值可以是start或end，指定每一步的值发生变化的时间点。第二个参数是可选的，默认值为end。
-        cubic-bezier(<number>, <number>, <number>, <number>)：
-        特定的贝塞尔曲线类型，4个数值需在[0, 1]区间内
-        */
-        transition-timing-function: ease-out;/*  过渡类型 */
-        transition-delay: .1s;/* 延迟执行时间  */
-    }
-    .menu-hide{
-        margin-left: -300px;
-    }
-</style>
