0f4517b8deecd4c4355d6d32bf87c73de089ed21
zhc <1009600394@qq.com>
2018-11-13 18:37:07 +0800

    登录框架界面调整好

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,40 +1,52 @@
 <template>
-    <v-form ref="form" v-model="valid" lazy-validation>
-        <v-text-field
-                v-model="name"
-                :rules="nameRules"
-                :counter="10"
-                label="输入用户名"
-                required
-        ></v-text-field>
-        <v-text-field
-                v-model="email"
-                :rules="emailRules"
-                label="E-mail"
-                required
-        ></v-text-field>
-        <v-select
-                v-model="select"
-                :items="items"
-                :rules="[v => !!v || 'Item is required']"
-                label="输入密码"
-                required
-        ></v-select>
-        <v-checkbox
-                v-model="checkbox"
-                :rules="[v => !!v || 'You must agree to continue!']"
-                label="同意条款?"
-                required
-        ></v-checkbox>
+    <v-layout justify-center>
+        <v-flex xs12 sm6  style="border: 1px solid #abacb1!important; border-radius: 5px;">
+            <v-toolbar color="indigo" dark>
 
-        <v-btn
-                :disabled="!valid"
-                @click="submit"
-        >
-            登录
-        </v-btn>
-        <v-btn @click="clear">清空</v-btn>
-    </v-form>
+                <v-toolbar-title>登录/注册</v-toolbar-title>
+                <v-spacer></v-spacer>
+
+            </v-toolbar>
+            <v-form ref="form" v-model="valid" lazy-validation>
+                <v-text-field
+                        v-model="name"
+                        :rules="nameRules"
+                        :counter="10"
+                        label="输入用户名"
+                        required
+                ></v-text-field>
+
+                <v-text-field
+                        v-model="email"
+                        :rules="emailRules"
+                        label="E-mail"
+                        required
+                ></v-text-field>
+                <v-select
+                        v-model="select"
+                        :items="items"
+                        :rules="[v => !!v || 'Item is required']"
+                        label="选择区域"
+                        required
+                ></v-select>
+                <v-checkbox
+                        v-model="checkbox"
+                        :rules="[v => !!v || 'You must agree to continue!']"
+                        label="同意条款?"
+                        required
+                ></v-checkbox>
+
+                <v-btn
+                        :disabled="!valid"
+                        @click="submit"
+                >
+                    登录
+                </v-btn>
+                <v-btn @click="clear">清空</v-btn>
+            </v-form>
+        </v-flex>
+    </v-layout>
+
 </template>
 <script>
     // import axios from 'axios'
@@ -55,10 +67,10 @@
             ],
             select: null,
             items: [
-                'Item 1',
-                'Item 2',
-                'Item 3',
-                'Item 4'
+                '北京',
+                '上海',
+                '广州',
+                '深圳'
             ],
             checkbox: false
         }),
