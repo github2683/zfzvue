9b65177b404c5468752f055b72a68c6136792776
zhanghc <1009600394@qq.com>
2019-07-08 17:53:46 +0800

    更新

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
+import './plugins/vuetify'
+
+import Vuetify from 'vuetify'
+// import 'vuetify/dist/vuetify.min.css'
+
+
 import App from './App.vue'
 
+
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 new Vue({
   render: h => h(App),
