02056c8b297c0bd8874f0b099605086a8f2713c4
zhanghc <1009600394@qq.com>
2019-07-13 14:51:49 +0800

    vuex vue-router

---
+++ vue002/src/views/Cart.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'cart',
+  data:function(){
+      return {
+        msg:'this is cart page'
+      }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
